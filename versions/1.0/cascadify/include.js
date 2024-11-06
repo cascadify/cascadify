a45fc8ff0c5741ecf3d7bd3d530cd150af9ad77b
TrollMii <140972023+TrollMii@users.noreply.github.com>
2024-11-06 17:07:45 +0100

    Update include.js

--- versions/1.0/cascadify/include.js
+++ versions/1.0/cascadify/include.js
@@ -1,5 +1,27 @@
-(function() {const basePath=(function(){const script=document.currentScript||(function(){const scripts = document.getElementsByTagName('script');return scripts[scripts.length - 1];})();return script.src.replace(/[^\/]+$/,'');})();
+(function() {
+    const basePath=(function(){
+        const script=document.currentScript||(function(){
+            const scripts = document.getElementsByTagName('script');
+            return scripts[scripts.length - 1];})();
+            return script.src.replace(/[^\/]+$/,'');
+        })();
     const requiredFiles = [
         'style.css'
     ];
-function loadScript(url){const script=document.createElement('script');script.src=basePath+url;script.type='text/javascript';script.async=true;document.head.appendChild(script);}requiredFiles.forEach(loadScript);})();
+function loadScript(url){
+    console.log("Load cascadify file: "+ url)
+    if (url.endsWith(".js")){
+        const script=document.createElement('script');
+        script.src=basePath+url;
+        script.type='text/javascript';
+        script.async=true;
+        document.head.appendChild(script);
+    } else if (url.endsWith(".css")){
+        const link=document.createElement('link');
+        link.href=basePath+url;
+        link.rel="stylesheet";
+        document.head.appendChild(link);
+    }
+}
+requiredFiles.forEach(loadScript);
+})();
